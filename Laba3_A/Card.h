bb58d75250522dbbe21efa31004cefbf28fab7eb
Vlad <vlad224@inbox.ru>
2020-10-05 19:03:22 +0300

    v1.0

---
+++ Laba3_A/Card.h
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<sstream>
+#include<cstdlib>
+#include<ctime>
+namespace Prog3 {
+	struct Card {
+		int rang;//ранг
+		int suit;//масть 0-пика 1-крести 2-бубна 3-черви
+	};
+	class DeckCard {
+	private:
+		static const int size = 10;
+		Card cards[size];
+	public:
+		DeckCard();
+		DeckCard(int a);
+		DeckCard(int r, int s);
+		int FindPlace(int r, int s)const;
+		int FindElement(int r, int s)const;
+		void Sort();
+		int getSize()const;
+		void quickSort_Rang(Card* a, int first, int last);
+		void quickSort_Suit(Card* a, long N);
+		DeckCard& add();
+		std::string Rang(int)const;
+		std::string Suit(int)const;
+		std::string rangget(int)const;
+		std::string suitget(int)const;
+        friend std::ostream& operator <<(std::ostream& c, const DeckCard& r);
+		friend std::istream& operator >>(std::istream& s, DeckCard& r);
+	};
+std::string dialog();
+void F_Add(DeckCard& r);
+void F_Random_DeckCard(DeckCard& r);
+void F_Suit(DeckCard& r);
+void F_Rang(DeckCard& r);
+void F_EnterNew(DeckCard& r);
+void F_Print(DeckCard& r);
+}
---
+++ Laba3_A/main.cpp
@@ -0,0 +1,29 @@
+#include "Card.h"
+using namespace Prog3;
+int main() {
+	srand((unsigned)time(0));
+	int b = 10;
+	void(*dp[7])(DeckCard & r) = { nullptr, Prog3::F_Add, Prog3::F_Print,Prog3::F_Suit, Prog3::F_Rang,Prog3::F_Random_DeckCard,Prog3::F_EnterNew };
+	DeckCard a(b);
+	a.Sort();
+	int fl1 = 1;
+	int c;
+	std::cout << a << std::endl;
+	while (fl1) {
+		std::cout << Prog3::dialog();
+		std::cin >> c;
+		if (!std::cin.good() || c < 0 || c > 6) {
+
+			std::cin.clear();
+
+			std::cin.ignore(INT_MAX, '\n');
+
+			std::cout << "Incorrect input!" << std::endl;
+		}
+		else
+			if (c == 0)
+				fl1 = 0;
+			else
+				dp[c](a);
+	}
+}
